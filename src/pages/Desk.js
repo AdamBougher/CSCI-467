6c95597d1452ede8fefc7d35727e3f6090f6307f
JuanLopez2004 <108855394+JuanLopez2004@users.noreply.github.com>
2024-11-12 18:05:41 -0600

    added row of identifiers to desk

--- src/pages/Desk.js
+++ src/pages/Desk.js
@@ -1,11 +1,32 @@
 import SiteNav from "../components/siteNav";
 import React from "react";
+import '../App.css';
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Button,
+} from "@mui/material";
 
 export default function Desk() {
     return (
       <body>
         <h1>Hi, this is the recieving desk page</h1>
         <SiteNav />
+
+        <Table class="warehouse">
+        <TableHead>
+          <TableRow>
+            <TableCell>Order#</TableCell>
+            <TableCell>Part Number</TableCell>
+            <TableCell>Desc</TableCell>
+            <TableCell>Quantity: </TableCell>
+          </TableRow>
+        </TableHead>
+        </Table>
+
       </body>
     );
 }
