3b21b0b4f4890144e5f9d27c9875ca273ed183c7
JuanLopez2004 <108855394+JuanLopez2004@users.noreply.github.com>
2024-11-14 23:09:34 -0600

    added part receiving file to display db info and minor changes to desk

--- src/pages/Desk.js
+++ src/pages/Desk.js
@@ -15,7 +15,6 @@ export default function Desk() {
       <body>
         <h1>Receiving Desk</h1>
         <SiteNav />
-
         <Table class="desk">
         <TableHead>
           <TableRow>
@@ -26,7 +25,6 @@ export default function Desk() {
           </TableRow>
         </TableHead>
         </Table>
-
       </body>
     );
 }
