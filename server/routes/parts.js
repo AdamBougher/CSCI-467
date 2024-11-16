a277693ed9126ee0f758e62180019a17db26dc4a
AdamBougher <a.bougher1999@gmail.com>
2024-11-16 01:22:29 -0600

    updated database from mareiadb to mysql

--- server/routes/parts.js
+++ server/routes/parts.js
@@ -1,44 +1,19 @@
-const express = require('express');
-const router = express.Router();
-const mysql = require("mysql");
+// var mysql = require("mysql");
+// var connection = mysql.createConnection({
+//   host: "blitz.cs.niu.edu",
+//   user: "student",
+//   password: "student",
+//   database: "csci467",
+// });
 
-async function connect() {
-  try {
-    const conn = await mysql.createConnection({
-      host: "blitz.cs.niu.edu",
-      user: "student",
-      password: "student",
-      database: "csci467",
-    });
-    console.log("Connected to the database");
-    return conn;
-  } catch (err) {
-    console.log("ERROR... CAN'T CONNECT: " + err);
-    throw err;
-  }
-}
+// connection.connect();
 
-async function getAllParts() {
-  let connection;
-  try {
-    connection = await connect();
-    const parts = await connection.query('SELECT * FROM parts');
-    return parts;
-  } catch (err) {
-    console.error("Error fetching parts: ", err);
-    throw err;
-  } finally {
-    if (connection) connection.end();
-  }
-}
-
-router.get("/api/parts", async (req, res) => {
-  try {
-    const parts = await getAllParts();
-    res.json(parts);
-  } catch (err) {
-    res.status(500).send("Error fetching parts");
-  }
-});
-
-module.exports = router;
+// module.exports = {
+//   getAll: async (result) => {
+//     connection.query("SELECT * FROM parts", function (err, rows) {
+//       if (err) throw err;
+//       console.log("rows: ", rows);
+//       result(rows);
+//     });
+//   },
+// };
--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
 const app = express();
-const parts = require("./routes/parts");
+app.use(cors());
+
+const connection = mysql.createConnection({
+  host: "blitz.cs.niu.edu",
+  user: "student",
+  password: "student",
+  database: "csci467",
+});
+
 
 const port = process.env.PORT || 8080;
 
-app.get("/api/parts", parts);
-
 app.get("/api/parts", (req, res) => {
-    parts.getAll((list) => {
-      res.send(list);
-    });
+  connection.query("SELECT * FROM parts", (err, results) => {
+    if (err) return res.json(err);
+      return res.json(results);
+  });
 });
 
 app.listen(port, () => {
