91e8fa9fffafe1c2cf110215dca623aec0a8ee42
AdamBougher <a.bougher1999@gmail.com>
2024-11-20 19:14:49 -0600

    removed unused code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,27 +12,6 @@ export default function Home() {
     <h1>Ryan's Private Parts</h1>
     </div>
       <SiteNav />
-      {/* <ItemCard
-        image="shitbox.jpg"
-        name="Shit box"
-        descr="Shit car lol"
-        cost="5.99"
-        quantity="20"
-      />
-      <ItemCard
-        image="porsche.jpg"
-        name="Porsche"
-        descr="Nice car"
-        cost="100000"
-        quantity="2"
-      />
-      <ItemCard
-        image="lambo.jpg"
-        name="Lambo"
-        cost="200000"
-        quantity="69"
-        descr="Lambo up!"
-      /> */}
       <Parts />
     </div>
   );
