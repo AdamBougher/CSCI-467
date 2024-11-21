7e71e5d2f65753fab3a8b23c826201ce9f80ca6d
AdamBougher <a.bougher1999@gmail.com>
2024-11-20 20:11:00 -0600

    removed unused code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import "../App.css";
 import React from 'react';
-import { ItemCard } from "../components/itemCard"; // Use named import
+
 import SiteNav from "../components/siteNav"; // Use default import
 import Parts from "../components/parts";
 
@@ -12,6 +12,27 @@ export default function Home() {
     <h1>Ryan's Private Parts</h1>
     </div>
       <SiteNav />
+      {/* <ItemCard
+        image="shitbox.jpg"
+        name="Shit box"
+        descr="Shit car lol"
+        cost="5.99"
+        quantity="20"
+      />
+      <ItemCard
+        image="porsche.jpg"
+        name="Porsche"
+        descr="Nice car"
+        cost="100000"
+        quantity="2"
+      />
+      <ItemCard
+        image="lambo.jpg"
+        name="Lambo"
+        cost="200000"
+        quantity="69"
+        descr="Lambo up!"
+      /> */}
       <Parts />
     </div>
   );
