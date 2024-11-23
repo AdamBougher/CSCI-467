e1de5fe5abeb3f3b6ccd6f40662900e688a02f75
Cameron456452 <camryan721@gmail.com>
2024-11-23 14:20:08 -0600

    Recieving Desk Inventory Component

--- src/pages/Desk.js
+++ src/pages/Desk.js
@@ -1,11 +1,18 @@
-import SiteNav from "../components/siteNav";
-import React from "react";
+import "../App.css";
+import React from 'react';
+import { Inventory } from "../components/inventory"; // Use named import
+import SiteNav from "../components/siteNav"; // Use default import
 
 export default function Desk() {
     return (
       <body>
         <h1>Hi, this is the recieving desk page</h1>
         <SiteNav />
+        <Inventory 
+          image="shitbox.jpg"
+          name="shitbox"
+          qty="10"
+        />
       </body>
     );
 }
