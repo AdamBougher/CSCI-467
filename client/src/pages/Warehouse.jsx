38dff8753869d6385e3daa4ed346fd4a9ef61d1b
AdamBougher <a.bougher1999@gmail.com>
2024-11-29 19:52:21 -0600

    re-added my code that keeps getting nuked

--- client/src/pages/Warehouse.jsx
+++ client/src/pages/Warehouse.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
-import SiteNav from "../components/siteNav";
-import Header from "../components/Header";
 import '../App.css';
-import Parts from "../components/parts";
+import { Table, TableBody, TableCell, TableHead, TableRow, Button } from '@mui/material';
 
 export default function Warehouse() {
   return (
     <body>
       <h2>Orders to Process</h2>
+      <Table class="warehouse">
+        <TableHead>
+          <TableRow>
+            <TableCell>Order#</TableCell>
+            <TableCell>Printable</TableCell>
+            <TableCell>Order Completed</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          <TableRow>
+            <TableCell>1</TableCell>
+
+            <TableCell className="printable">
+              <Button
+                size="small"
+                variant="contained"
+                onClick={() => window.print()}
+              >
+                Packing list
+              </Button>
+              <Button
+                size="small"
+                variant="contained"
+                onClick={() => window.print()}
+              >
+                Invoice
+              </Button>
+              <Button
+                size="small"
+                variant="contained"
+                onClick={() => window.print()}
+              >
+                Shipping label
+              </Button>
+            </TableCell>
+
+            <TableCell>
+              <Button variant="contained">Order Shipped</Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </body>
+    
   );
 }
