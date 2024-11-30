d7a55e2e09fcfe1f1e688d07a7320d1410c8ce46
AdamBougher <a.bougher1999@gmail.com>
2024-11-29 23:40:05 -0600

    removed unused imports

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import "../App.css";
 import React from 'react';
-import SiteNav from "../components/siteNav"; // Use default import
-import Header from "../components/Header";
 import Parts from "../components/parts";
 
 export default function Home() {
   return (
-    <div>
+    <section className="parts-container">
       <Parts />
-    </div>
+    </section>
   );
 }
