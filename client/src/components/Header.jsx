e1bd9cf58011caaf11a88060ea7e4c4a844a905d
AdamBougher <a.bougher1999@gmail.com>
2024-11-29 20:08:16 -0600

    added links to logo and cart icon

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
       <header className="header">
         
-        <div className="header-logo">
-          <img src="/app-logo.png" alt="Logo" />
-        </div>
-  
-        <div className="shopping-cart-container">
-          <img 
-            src="/shopping-cart.png" 
-            alt="Cart" 
-            className="shopping-cart" 
-          />
-        </div>
+        <Link to="/">
+          <div className="header-logo">
+            <img src="/app-logo.png" alt="Logo" />
+          </div>
+        </Link>
+
+        <Link to="/Checkout">
+          <div className="shopping-cart-container">
+            <img 
+              src="/shopping-cart.png" 
+              alt="Cart" 
+              className="shopping-cart" 
+            />
+          </div>
+        </Link>
       </header>
     );
   };
