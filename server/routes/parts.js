0d3ac65c25cdb9366b3677620d40b4717b8c8bbc
AdamBougher <a.bougher1999@gmail.com>
2024-11-29 23:59:18 -0600

    updated to mysql2

--- server/routes/parts.js
+++ server/routes/parts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: "blitz.cs.niu.edu",
