7b0cd11a5bf9f794306dc794ff4b96a34d1fdbcf
AdamBougher <a.bougher1999@gmail.com>
2024-11-30 14:32:54 -0600

    removed code that wasn't doing dick

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const parts = require("./routes/parts");
+const parts = require("./routes/legacyDB");
 const cors = require("cors");
 const coresOptions = {
   origin: "http://localhost:5173",
@@ -11,12 +11,6 @@ const port = process.env.PORT || 8080;
 app.use(cors(coresOptions));
 
 app.get("/api/parts", parts);
-
-app.get("/api/parts", (req, res) => {
-    parts.getAll((list) => {
-      res.send(list);
-    });
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
