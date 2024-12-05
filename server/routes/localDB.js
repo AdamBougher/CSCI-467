1cbde3aeccd3ec8cf8b1ef0ac503ed03fad61868
AdamBougher <a.bougher1999@gmail.com>
2024-12-04 20:28:56 -0600

    fixed path name

--- server/routes/localDB.js
+++ server/routes/localDB.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Construct the correct path to the partsDb.sqlite file
-const dbPath = path.join(__dirname, '..', 'db', 'partsDb.sqlite');
+const dbPath = path.join(__dirname, '..', 'db', 'partsdb.sqlite');
 const db = new sqlite3.Database(dbPath);
 
 // Remote database connection pool
